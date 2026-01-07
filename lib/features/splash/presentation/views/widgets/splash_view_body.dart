aa3b19ca9f315f52171ea47b43b8946e1c3a696f
mora11111 <mohanad1481979@gmail.com>
2026-01-07 17:26:32 +0200

    splash view logic done and navigate to on boarding

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:svg_flutter/svg.dart' show SvgPicture;
 
 import '../../../../../core/utils/app_images.dart' show Assets;
+import '../../../../on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+
+  @override
+  void initState() {
+    executeNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,4 +34,12 @@ class SplashViewBody extends StatelessWidget {
       ],
     );
   }
+
+  void executeNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+  }
 }
+
+
