0f74512a14d30d3405d679bc6bf59c48ec5de6e9
mora11111 <mohanad1481979@gmail.com>
2026-01-07 15:53:08 +0200

    feat: Add splash screen and setup routing

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_application/features/splash/presentation/views/splash_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+
+
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_application/screens/home_screen.dart';
+import 'package:fruits_application/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_application/features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
     );
   }
 }
