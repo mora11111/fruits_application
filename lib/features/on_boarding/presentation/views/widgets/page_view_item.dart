0a886651358be8d15ff51738e99aca2e75a65511
mora11111 <mohanad1481979@gmail.com>
2026-01-24 01:07:10 +0200

    Implement on-boarding page view item widget

---
+++ lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg.dart';
+
+class PageViewItem extends StatelessWidget {
+  const PageViewItem({
+    super.key,
+
+    required this.subtitle,
+    required this.image,
+    required this.backgroundImage,
+    required this.title,
+  });
+
+  final Widget title;
+  final String subtitle, image, backgroundImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: SvgPicture.asset(backgroundImage, fit: BoxFit.fill),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: SvgPicture.asset(image),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: const Text('تخط'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
