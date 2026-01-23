723ab2dd354762b366aefa6821c8f1ac55687754
mora11111 <mohanad1481979@gmail.com>
2026-01-24 01:08:05 +0200

    setup localization with English and Arabic support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_application/core/helper_functions/on_generate_routes.dart';
 import 'package:fruits_application/features/splash/presentation/views/splash_view.dart';
+import 'package:fruits_application/generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
+      locale: const Locale('ar'),
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
     );
