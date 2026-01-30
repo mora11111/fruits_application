fd5c038c2f51f50909dd098814b325148f34706f
mora11111 <mohanad1481979@gmail.com>
2026-01-30 22:42:54 +0200

    Add DotsIndicator to OnBoardingViewBody and define primary color

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,11 +1,22 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_application/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
+
+import '../../../../../core/utils/app_colors.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Expanded(child: OnBoardingPageView())]);
+    return Column(
+      children: [
+        Expanded(child: OnBoardingPageView()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(activeColor: AppColors.primaryColor),
+        ),
+      ],
+    );
   }
 }
